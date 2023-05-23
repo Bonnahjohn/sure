64c287eea86601a49ca11ca7d3a8b56610b0da9d
John Bonnah <bonnahpoku@gmail.com>
2023-05-23 05:46:02 -0700

    price and size has been rmove froom the class properties

--- sure/lib/screen/config.dart
+++ sure/lib/screen/config.dart
@@ -3,14 +3,13 @@
 import 'package:flutter/material.dart';
 
 class Product {
-  late String image, title, description;
+  late String image, title, science;
   late int price, size;
   late Color color;
   Product({
+    //required this.science
     required this.image,
     required this.title,
-    required this.price,
-    required this.size,
   });
 }
 
@@ -18,241 +17,161 @@ List<Product> products = [
   Product(
     image: 'assets/images/fast.png',
     title: 'Apple',
-    price: 200,
-    size: 30,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Pineapple',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Coconut',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
-    title: 'Tomatoes',
-    price: 304,
-    size: 50,
+    title: 'Mango',
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Grapes',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Orange',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Pawpaw',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Apple',
-    price: 200,
-    size: 30,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Pineapple',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Coconut',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Tomatoes',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Grapes',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Orange',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Pawpaw',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   ),
   Product(
     image: 'assets/images/fast.png',
     title: 'Guava',
-    price: 304,
-    size: 50,
   )
 ];
